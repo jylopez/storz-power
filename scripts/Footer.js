d4fb9c67a2b096545a28e1179b971e0a35483641
jylopez <jershonlopez@gmail.com>
2018-09-22 09:31:50 -0600

    Fixed footer

--- scripts/Footer.js
+++ scripts/Footer.js
@@ -1,7 +1,7 @@
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer"><span>© 2018 Watthub. All Rights Reserved.</span></div>
+      <div className="footer"><span>© 2018 Storz Power. All Rights Reserved.</span></div>
     )
   }
 }
