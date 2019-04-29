519784502564a80f9743eceed8ada8183ce9e4d3
jylopez <jershonlopez@gmail.com>
2019-04-29 10:49:00 -0600

    Change to 10MW

--- scripts/Utility.js
+++ scripts/Utility.js
@@ -6,7 +6,7 @@ class Utility extends React.Component {
         <div className="title">
           <span className="bold">UTILITY</span> <span>SCALE</span>
         </div>
-        <div className="description"><span className="bold">Storz Power</span> Nano Printed Lithium maximizes power density while reducing the physical footprint. This puts us at 30% of the size of the competition. <span className="bold">Storz Power</span> puts 40 MW into a 40-foot container and 4 MW into a 20-foot container.</div>
+        <div className="description"><span className="bold">Storz Power</span> Nano Printed Lithium maximizes power density while reducing the physical footprint. This puts us at 30% of the size of the competition. <span className="bold">Storz Power</span> puts 10 MW into a 40-foot container and 4 MW into a 20-foot container.</div>
       </div>
     )
   }
